9161baacc952e80e2d441303f39c6ed6aab314b8
bellezza <bellezza@spaziodati.eu>
2024-09-27 11:21:05 +0200

    first commit

---
+++ geoarch.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import folium
+from streamlit_folium import st_folium
+
+
+def plot_data():
+    df = pd.read_csv(
+        "https://docs.google.com/spreadsheets/d/"
+        + "1Ie9ucp2dlNpTx3s-mJv9u__MJykDy2p7ShcHZVM7Hws"
+        + "/export?gid=0&format=csv",
+        converters={"lat": np.float64, "lon": np.float64},
+    )
+
+    unique_types = df.type.unique()
+    color_palette = sns.color_palette("bright", len(unique_types))
+    color_palette = [
+        "red",
+        "blue",
+        "green",
+        "purple",
+        "orange",
+        "darkred",
+        "lightred",
+        "beige",
+        "darkblue",
+        "darkgreen",
+        "cadetblue",
+        "darkpurple",
+        "white",
+        "pink",
+        "lightblue",
+        "lightgreen",
+        "gray",
+        "black",
+        "lightgray",
+    ]
+    color_map = dict(zip(unique_types, color_palette))
+    df["color"] = df.type.map(color_map)
+
+    icon_list = [
+        "search",
+        "home",
+        "leaf",
+        "cog",
+        "cloud",
+        "star",
+        "glass",
+        "music",
+        "heart",
+        "film",
+        "flag",
+        "book",
+    ]
+    icon_map = dict(zip(unique_types, icon_list))
+    df["icon"] = df.type.map(icon_map)
+
+    # sidebar
+    with st.sidebar:
+        filters = dict()
+        with st.form("Params"):
+            left, _, right = st.columns(3)
+            with left:
+                st.subheader("Params")
+            with right:
+                st.form_submit_button(label="Apply Filters", type="primary")
+
+            filters["type"] = st.multiselect("Tipo", unique_types, default=unique_types)
+
+    # main
+    tab1_map, tab2_data = st.tabs(["Map", "Data"])
+
+    with tab1_map:
+        data = df[df.type.isin(filters["type"])]
+        location = data[["lat", "lon"]].mean().values.tolist()
+
+        # center on Liberty Bell, add marker
+        m = folium.Map(location=location, zoom_start=8)
+        for i, row in data.iterrows():
+            folium.Marker(
+                location=[row["lat"], row["lon"]],
+                popup=row["name"],
+                tooltip=row["type"],
+                icon=folium.Icon(color=row["color"], icon=row["icon"]),
+            ).add_to(m)
+
+        # call to render Folium map in Streamlit
+        st_data = st_folium(m, width=725)
+
+    with tab2_data:
+        st.write(df[["name", "type", "lat", "lon"]])
+
+
+if __name__ == "__main__":
+    plot_data()
